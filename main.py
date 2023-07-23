9bdbfe24908bd03c727fd6be8ed5bf6419ddd842
caregor <caregor@gmail.com>
2023-07-23 13:33:30 +0500

    Task 1_1 is done. Added main.py for results;

---
+++ main.py
@@ -0,0 +1,10 @@
+import task1_1
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    date = '29.02.2001'
+    if task1_1.is_valid_date(date):
+        print(f"{date} - Дата существует.")
+    else:
+        print(f"{date} - Некорректная дата.")
--- task1_1.py
+++ task1_1.py
@@ -6,3 +6,27 @@
 Весь период (1 января 1 года - 31 декабря 9999 года) действует Григорианский календарь.
 Проверку года на високосность вынести в отдельную защищённую функцию.
 """
+
+
+def _is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def is_valid_date(date_string):
+    try:
+        day, month, year = map(int, date_string.split('.'))
+        if 1 <= year <= 9999 and 1 <= month <= 12 and 1 <= day <= 31:
+            if month == 2 and day == 29:
+                return _is_leap_year(year)
+
+            if month in [4, 6, 9, 11] and day == 31:
+                return False
+
+            if month == 2 and day > 29:
+                return False
+
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
